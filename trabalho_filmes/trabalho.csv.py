a15f307c3da11493e968d639e408bf1a92ff97f1
brunarodrigues11 <rodriguesfurtadob@gmail.com>
2026-04-03 21:30:27 -0300

    Add files via upload

--- trabalho_filmes/trabalho.csv.py
+++ trabalho_filmes/trabalho.csv.py
@@ -4,6 +4,7 @@ rt = []
 mr = []
 yr = []
 gr = []
+mn = []
 
 with open('data.csv', 'r',
           encoding='utf-8') as arquivo:
@@ -19,7 +20,8 @@ with open('data.csv', 'r',
         mr.append(float(linha['Movie Rating']))
         yr.append(int(linha['Year of Release']))
         gr.append(linha['Genre'])
-
+        mn.append(linha['Movie Name'])
+        
         if cont == 1:
             titulo = linha
         elif cont <= 5:
@@ -38,28 +40,43 @@ print("Maior tempo de filme: " , max(rt))
 print("Maior avaliação: " , max(mr))
 
 mediart = sum(rt) / len(rt)
-print(mediart) 
+print("Média do tempo de filme: ", mediart)
 
 mediamr = sum(mr) / len(mr)
-print(mediamr)
+print("Média da avaliação: ", mediamr)
 
 
-#  FILTROS   (fazer + 2)     
+#  FILTROS     
 
 indice = 0
-for y in yr:
-    if y == 1950 and mr[indice] >= 8.0:     # filmes do ano de 1950 e com notas acima de 8.0 
-        print(y, mr[indice], indice)
-    indice +=1
+contador = 0
 
-for m in mr:
-    if m >= 5.0:     # filmes com notas acima de 5.0
-        print(m, indice)
-    indice +=1
+for i, y in enumerate(yr):   # filmes do ano de 1950 e com notas acima de 8.0
+    if y == 1950 and mr[i] >= 8.0:
+        print(f"Título: {titulos[i]} | Ano: {y} | Nota: {mr[i]}")
+        contador += 1
 
-for g in gr:
-    input_genre = input("Digite um gênero de filme: ")
-    if g == input_genre:     # filmes do gênero digitado pelo usuário
-        print(g, indice)
-    indice +=1
-    
+        if contador == 10:
+            break
+
+contador = 0
+
+for i, m in enumerate(mr):   # filmes com notas acima de 9.0
+    if m >= 9.0:          
+        print(f"Nome: {mn[i]} | Nota: {m}")
+        contador += 1
+
+        if contador == 10:
+            break
+
+input_genre = input("Digite um gênero de filme: ").strip().lower()
+
+contador = 0
+
+for i, g in enumerate(gr):   # filmes do gênero digitado pelo usuário
+    if input_genre in g.lower():
+        print(f"Nome: {mn[i]} | Gênero: {g} |")
+        contador += 1
+        
+        if contador == 10:
+            break
